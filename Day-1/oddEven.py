f0e480a50acd9673ba68fa52333f6279dfbcd19b
Abhishek Gautam <83368305+Abhigautam6517@users.noreply.github.com>
2022-08-16 21:45:49 +0530

    Add files via upload

---
+++ Day-1/oddEven.py
@@ -0,0 +1,6 @@
+num = int(input("Enter your digit"))
+
+if(num%2==0):
+    print("This number is even")
+else:
+    print("THis number is odd")
---
+++ Day-1/printTable.py
@@ -0,0 +1,3 @@
+num = int(input("Enter your table : "))
+for i in range(1,11):
+    print(num, 'X' ,i,"=",num*i)
